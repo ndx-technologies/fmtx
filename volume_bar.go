43161d35d88ca4f6f7471803aeba31106577c5f4
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2026-08-11 15:17:37 +0700

    fix

--- volume_bar.go
+++ volume_bar.go
@@ -16,6 +16,12 @@ func VolumeBar2[T int | int32 | int64](a, b, max T, width int) string {
 	if max <= 0 {
 		return ""
 	}
+	if a < 0 {
+		a = 0
+	}
+	if b < 0 {
+		b = 0
+	}
 	barA := min(width, int(float64(a)*float64(width)/float64(max)))
 	barB := min(width-barA, int(float64(b)*float64(width)/float64(max)))
 	return BlueS(strings.Repeat("█", barA) + strings.Repeat("▒", barB) + strings.Repeat("░", width-barA-barB))
